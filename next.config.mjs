d86bf511b8fe51440a03170605437d710ed12e38
emojih <emmanuelojih112@gmail.com>
2025-01-21 12:52:39 +0100

    ..,

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    // basePath: '/Portfolio', // Replace 'your-repo-name' with the actual repository name
-    // assetPrefix: '/Portfolio',
+    output: 'export',
+    basePath: '/Portfolio', // Replace 'your-repo-name' with the actual repository name
+    assetPrefix: '/Portfolio',
   };
   
   export default nextConfig;
